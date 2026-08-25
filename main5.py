7458ae57242b79c745139c33146b0ed0de3fd08d
Jibran Mohamed <junaidtheboss220@gmail.com>
2026-08-25 19:22:47 +0100

    unpack

--- main5.py
+++ main5.py
@@ -1,5 +1,13 @@
 x,y,z = "dragon fruit", "apple","durian"
 print (x,y,z)
 x,y,z = "orange"
+# unpacking
 fruits = ["dragon fruit", "apple","durian"]
 x,y,z = fruits
+
+# make sure the number of variables matches the number of values
+
+
+yourChoice = ["friday" , "saturday" , "blarday hell mate" ]
+
+x,y = yourChoice
