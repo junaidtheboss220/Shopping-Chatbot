e3599689b589ec202429bca3db3b035c1b3ed897
Jibran Mohamed <junaidtheboss220@gmail.com>
2026-09-08 19:21:02 +0100

    datatype

--- test.py
+++ test.py
@@ -1,5 +1,29 @@
 Name = "fortt"
 
 print (Name)
-
 print (type(Name)) 
+
+# int
+
+age = 0
+print (age)
+print (type(age))
+
+height = 5.1
+print (type(height))
+
+shopping_list = ["milk","ice","fork"]
+print (shopping_list)
+print (type(shopping_list))
+
+order_history= ("milk","ice","fork")
+print(order_history)
+print (type (order_history))
+
+personal_details={
+    "name":"mohammed musab khan",
+    "age":89,
+    "height":167
+}
+print (personal_details)
+print (type(personal_details))
